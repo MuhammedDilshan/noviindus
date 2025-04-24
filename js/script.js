3176ffbd0bfc750e0e3504261213e5c9bfef4748
dilshanmambadan <dilshanmambadan@gmail.com>
2025-04-24 10:31:07 +0530

    responsive completede

--- js/script.js
+++ js/script.js
@@ -15,3 +15,68 @@ accordionItems.forEach((item) => {
     item.classList.toggle("active");
   });
 });
+
+new Swiper(".subjects-swiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500, // 2.5 seconds between slides
+    disableOnInteraction: false, // keeps autoplay after user interacts
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: { slidesPerView: 3 },
+    768: { slidesPerView: 4 },
+    1024: { slidesPerView: 5 },
+  },
+});
+
+new Swiper(".kids_list.swiper", {
+  slidesPerView: 1,
+  spaceBetween: 26,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.classList.toggle("active");
+}
+
+const swiper = new Swiper(".toeflSlider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500, // 2.5 seconds between slides
+    disableOnInteraction: false, // keeps autoplay after user interacts
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
+  },
+});
